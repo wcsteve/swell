8b580236fe9e7367b668473aa2b94ae1a5b0f6bf
wcsteve <wcsteve9@gmail.com>
2018-04-12 20:35:19 -0700

    Fix migrations

--- db/migrate/20180412210421_add_column_workout.rb
+++ db/migrate/20180412210421_add_column_workout.rb
@@ -1,6 +1,6 @@
-class AddColumnWorkout < ActiveRecord::Migration[5.1]
-  def change
-    add_column :workouts, :workout_time_hours, :string, null: false
-    add_column :workouts, :workout_time_minutes, :string, null: false
-  end
-end
+# class AddColumnWorkout < ActiveRecord::Migration[5.1]
+#   def change
+#     add_column :workouts, :workout_time_hours, :string, null: false
+#     add_column :workouts, :workout_time_minutes, :string, null: false
+#   end
+# end
