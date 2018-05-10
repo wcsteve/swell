1e5b799c28f10e070d57b47a861e9857f5e289b0
wcsteve <wcsteve9@gmail.com>
2018-05-10 11:19:36 -0700

    Create Stats controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show]
     resources :routes, only: [:index, :show, :create, :update, :destroy]
     resources :workouts, only: [:index, :show, :create, :update, :destroy]
-    resources :workouts do
-      get 'stats', on: :collection
-    end
+    resources :stats, only: [:show, :index]
   end
 
   root "static_pages#root"
